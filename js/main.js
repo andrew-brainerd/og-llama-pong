5e5e6c63f950d37781f43009cef49c839a641cc9
Andrew Brainerd <drwb333@gmail.com>
2018-01-19 01:50:42 -0500

    Added click triggers, game start functionality

--- js/main.js
+++ js/main.js
@@ -2,19 +2,43 @@ var leftPointCounter = 0;
 var rightPointCounter = 0;
 var left = 0;
 var right = 1;
-var server = left;
+var server;
+var gameStarted = false;
+
+$("#leftPointCount").click(function() {
+    IncrementLeftScore();
+});
+
+$("#rightPointCount").click(function () {
+    IncrementRightScore();
+});
 
 if (annyang) {
+    //SaySomething("Welcome to LLama Pong. Voice commands are enabled. Let's get started");
+    SaySomething("Who is serving first, left or right?");
+
     var commands = {
-        "point left": function () {
-            leftPointCounter++;
-            $("#leftPointCount").text(leftPointCounter);
-            ProcessScore(leftPointCounter, rightPointCounter);
+        "left": function() {
+            if (!gameStarted) {
+                server = left;
+                gameStarted = true;
+                $("#leftPointCount").addClass("serving");
+                SaySomething("Left is serving first. Let's start the game");
+            }  
         },
-        "point right": function () {
-            rightPointCounter++;
-            $("#rightPointCount").text(rightPointCounter);
-            ProcessScore(leftPointCounter, rightPointCounter);
+        "right": function() {
+            if (!gameStarted) {
+                server = right;
+                gameStarted = true;
+                $("#rightPointCount").addClass("serving");
+                SaySomething("Right is serving first. Let's start the game");
+            }  
+        },
+        "point left": function() {
+            IncrementLeftScore();
+        },
+        "point right": function() {
+            IncrementRightScore();
         }
     };
     annyang.debug();
@@ -28,12 +52,31 @@ if (annyang) {
     annyang.start({ autoRestart: true, continuous: true });
 } else page.css("background", "#001E3D");
 
+function IncrementRightScore() {
+    rightPointCounter++;
+    $("#rightPointCount").text(rightPointCounter);
+    ProcessScore(leftPointCounter, rightPointCounter);
+}
+
+function IncrementLeftScore() {
+    leftPointCounter++;
+    $("#leftPointCount").text(leftPointCounter);
+    ProcessScore(leftPointCounter, rightPointCounter);
+}
+
 function ProcessScore(leftScore, rightScore) {
     isMultipleOfFive = (leftScore + rightScore) % 5 == 0;
     isGreaterThanZero = (leftScore + rightScore) > 0;
 
     if (isMultipleOfFive && isGreaterThanZero) {
         server = !server;
+        if (server == left) {
+            $("#rightPointCount").removeClass("serving");
+            $("#leftPointCount").addClass("serving");
+        } else {
+            $("#leftPointCount").removeClass("serving");
+            $("#rightPointCount").addClass("serving");
+        }
         SaySomething("Switch server");    
     }
 
