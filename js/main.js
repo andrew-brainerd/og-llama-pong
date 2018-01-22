2f4456503a902cdc411a07fcbd6430d278dffb6a
Andrew Brainerd <drwb333@gmail.com>
2018-01-21 19:51:51 -0500

    Improved scoring

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var server;
 var gameStarted = false;
 var gameEnded = false;
 
+$(".point-counter").text(0);
+
 $("#leftPointCount").click(function() {
     IncrementLeftScore();
 });
@@ -14,25 +16,37 @@ $("#rightPointCount").click(function () {
     IncrementRightScore();
 });
 
+$(document).keydown(function (e) {
+    if (e.which == 37) { // left
+        if (!gameStarted) {
+            StartTheGame(left);
+        } else {
+            IncrementLeftScore();
+        }
+        
+    }
+    else if (e.which == 39) { // right
+        if (!gameStarted) {
+            StartTheGame(left);
+        } else {
+            IncrementRightScore();
+        }
+    }
+});
+
 if (annyang) {
-    SaySomething("Welcome to LLama Pong. Let's get started");
+    SaySomething("Welcome to laama Pong. Let's get started");
     SaySomething("Who is serving first, left or right?");
 
     var commands = {
         "left": function() {
             if (!gameStarted) {
-                server = left;
-                gameStarted = true;
-                $("#leftPointCount").addClass("serving");
-                SaySomething("Left is serving first. Let's start the game");
+                StartTheGame(left);
             }  
         },
         "right": function() {
             if (!gameStarted) {
-                server = right;
-                gameStarted = true;
-                $("#rightPointCount").addClass("serving");
-                SaySomething("Right is serving first. Let's start the game");
+                StartTheGame(left);
             }  
         },
         "point left": function() {
@@ -57,6 +71,15 @@ if (annyang) {
     annyang.start({ autoRestart: true, continuous: true });
 } else page.css("background", "#001E3D");
 
+function StartTheGame(startingServer, ele, message) {
+    server = startingServer;
+    var side = (server == left) ? "left" : "right";
+
+    $("#" + side + "PointCount").addClass("serving");
+    SaySomething(side + "is serving first. Let's start the game.");
+    gameStarted = true;
+}
+
 function IncrementRightScore() {
     rightPointCounter++;
     $("#rightPointCount").text(rightPointCounter);
@@ -72,16 +95,28 @@ function IncrementLeftScore() {
 function ProcessScore(leftScore, rightScore) {
     isMultipleOfFive = (leftScore + rightScore) % 5 == 0;
     isGreaterThanZero = (leftScore + rightScore) > 0;
+    iSOverTwentyOne = (leftScore >= 21 || rightScore >= 21);
+
+    if (iSOverTwentyOne) {
+        var greaterScore = (leftScore > rightScore) ? leftScore : rightScore;
+        var lesserScore = (greaterScore == leftScore) ? rightScore : leftScore;
+
+        if ((greaterScore - lesserScore) >= 2) {
+            if (greaterScore == leftScore) {
+                player = "Left";
+            } else {
+                player = "Right";
+            }
+
+            gameEnded = true;
+            $("body").empty().append("<h1>" + player + " is the winner!</h1>");
+            return;
+        }
+    }
 
     if (isMultipleOfFive && isGreaterThanZero) {
         server = !server;
-        if (server == left) {
-            $("#rightPointCount").removeClass("serving");
-            $("#leftPointCount").addClass("serving");
-        } else {
-            $("#leftPointCount").removeClass("serving");
-            $("#rightPointCount").addClass("serving");
-        }
+        $(".point-counter").toggleClass("serving");
         SaySomething("Switch server");    
     }
 
@@ -93,16 +128,5 @@ function ProcessScore(leftScore, rightScore) {
         receiverScore = leftPointCounter;
     }
 
-    SaySomething("The score is now " + serverScore + " serving " + receiverScore);
-
-    if (leftScore == 21 || rightScore == 21) {
-        if (leftScore == 21) {
-            player = "Left";
-        } else {
-            player = "Right";
-        }
-
-        gameEnded = true;
-        $("body").empty().append("<h1>" + player + " is the winner!</h1>");
-    }
+    //SaySomething("The score is now " + serverScore + " serving " + receiverScore);   
 }
